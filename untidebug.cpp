1269eb1e9974673e26a312549d8e77690094440c
Ivan <ivannechta@gmail.com>
2022-05-17 19:26:50 +0700

    Init

---
+++ untidebug.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "DebugDetect.h"
+using namespace std;
+
+int main()
+{
+    if (f01()) cout << "Method 01: Detected Debugger\n";
+    if (f02()) cout << "Method 02: Detected Debugger\n";
+    if (f03()) cout << "Method 03: Detected Debugger\n";
+    if (f04()) cout << "Method 04: Detected Debugger\n";
+    if (f05()) cout << "Method 05: Detected Debugger\n";
+
+    cout << "Put VisualStudio Breakpoint here\n";
+}
+
